cb4a69c7b9acfb58c18c3a950bbd3bb4f6f26f2c
ceeriil <simonorugbo15@gmail.com>
2025-07-03 07:39:42 +0100

    connect wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
-import { type FC, useEffect, useMemo, Suspense } from "react";
-import type { AppProps } from "next/app";
-import { useRouter as useNavigationRouter } from "next/navigation";
-import { useRouter } from "next/router";
-import { Toaster } from "react-hot-toast";
-import { ErrorBoundary } from "@/components/ErrorBoundary";
-import { Loader } from "@/components/Loader";
-import "@/styles/globals.css";
-import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
-import { base } from "wagmi/chains";
+import { type FC, Suspense, useEffect, useMemo } from 'react';
+
+import type { AppProps } from 'next/app';
+import { useRouter as useNavigationRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { Loader } from '@/components/Loader';
+import '@/styles/globals.css';
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
+import { Toaster } from 'react-hot-toast';
+import { base } from 'wagmi/chains';
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
   <div>
     <p>An unhandled error occurred:</p>
     <blockquote>
-      <code>{error instanceof Error ? error.message : typeof error === "string" ? error : JSON.stringify(error)}</code>
+      <code>{error instanceof Error ? error.message : typeof error === 'string' ? error : JSON.stringify(error)}</code>
     </blockquote>
   </div>
 );
@@ -31,7 +33,17 @@ const BackButtonManipulator: FC = () => {
 const App: FC<AppProps> = ({ pageProps, Component }) => {
   return (
     <>
-      <MiniKitProvider chain={base}>
+      <MiniKitProvider
+        chain={base}
+        apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+        config={{
+          appearance: {
+            mode: 'auto',
+            theme: 'mini-app-theme',
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+          },
+        }}
+      >
         <Suspense fallback={<Loader />}>
           <BackButtonManipulator />
           <main className="relative bgcover overflowxhidden" style={{ background: `url('/img/stars.svg')` }}>
@@ -51,12 +63,12 @@ const Inner: FC<AppProps> = props => {
 
   useEffect(() => {
     if (debug) {
-      let el = document.createElement("div");
+      let el = document.createElement('div');
       document.body.appendChild(el);
-      import("eruda").then(lib =>
+      import('eruda').then(lib =>
         lib.default.init({
           container: el,
-          tool: ["console", "elements"],
+          tool: ['console', 'elements'],
         }),
       );
     }
